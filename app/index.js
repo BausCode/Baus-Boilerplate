67a40586981b37484e2daaa4e5710b6e31c3dbb8
Gabriel Walford <gabriel@bauscode.com>
2015-11-23 14:43:08 -0700

    setup for hot reload when compatible with babel6

--- app/index.js
+++ app/index.js
@@ -3,4 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
 import '../public/css/app.scss';
 
+console.log('%c App Started', 'color:green');
+
 ReactDOM.render(<App />, document.getElementById('main'));
