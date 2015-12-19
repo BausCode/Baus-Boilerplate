441c27073422905fe26edfe88f336ef3bd356b22
Gabriel Walford <gabriel@bauscode.com>
2015-12-19 08:22:55 -0700

    updated router to work with index.html

--- app/routes.js
+++ app/routes.js
@@ -1,17 +1,15 @@
+import React from 'react';
+import { Route, IndexRoute } from 'react-router';
 import App from './components/App';
 import Home from './components/Home';
 import Counter from './components/Counter';
 import NoMatch from './components/NoMatch';
 
-export default [
-  {
-    path: '/',
-    component: App,
-    indexRoute: { component: Home },
-    childRoutes: [
-      { path: '/', component: Home },
-      { path: '/counter', component: Counter },
-      { path: '*', component: NoMatch }
-    ]
-  }
-];
+export default (
+  <Route path="/" component={App}>
+    <IndexRoute component={Home} />
+    <Route path="/index.html" component={Home} />
+    <Route path="/counter" component={Counter} />
+    <Route path="/*" component={NoMatch} />
+  </Route>
+);
