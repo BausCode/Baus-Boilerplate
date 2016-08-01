7b6feb3e3f2db4c508bb3c4239319fc0e6429caf
Gabriel Walford <gabriel@bauscode.com>
2016-08-01 14:36:55 -0600

    updated app passing state

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,8 +1,8 @@
 import { combineReducers } from 'redux';
 import counter from './counter';
 
-const rootReducer = combineReducers({
+const reducers = {
   counter
-});
+};
 
-export default rootReducer;
+export default combineReducers(reducers);
